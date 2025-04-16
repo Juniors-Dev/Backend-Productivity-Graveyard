d600f127d5c8b59f51699d4a97f6b241b22579dc
Anclagen <91462616+Anclagen@users.noreply.github.com>
2025-04-16 21:04:25 +0200

    added db seeder for roles and types

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,23 @@
 var express = require("express");
 var router = express.Router();
+var { db } = require("../models");
 
 router.get("/", function (req, res, next) {
   res.status(200).json({ message: "Welcome to the API" });
+});
+
+router.get("/check", async function (req, res, next) {
+  try {
+    const roles = await db.Role.findAll();
+    const types = await db.Type.findAll();
+    const users = await db.User.findAll();
+    const projects = await db.Project.findAll();
+
+    res.status(200).json({ status: "ok", roles, types, users, projects });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ status: "error", message: "Internal Server Error" });
+  }
 });
 
 module.exports = router;
