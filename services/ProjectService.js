72d972a7ebcee088f88a78a176b2bbc0109f9f55
Anclagen <91462616+Anclagen@users.noreply.github.com>
2025-04-17 21:34:32 +0200

    project services added

--- services/ProjectService.js
+++ services/ProjectService.js
@@ -2,19 +2,84 @@ class ProjectService {
   constructor(db) {
     this.client = db.sequelize;
     this.Project = db.Project;
+    this.ProjectTag = db.ProjectTag;
+    this.Type = db.Type;
   }
 
   async getAll() {
-    return this.Project.findAll();
+    return this.Project.findAll({
+      include: [
+        {
+          model: this.Type,
+          as: "types",
+          through: { attributes: [] },
+        },
+      ],
+    });
   }
 
-  async create({ name, description, startDate, endDate }) {
-    return this.Project.create({
-      name,
-      description,
-      startDate,
-      endDate,
+  async getOneId(id) {
+    const project = await this.Project.findByPk(id, {
+      include: [
+        {
+          model: this.Type,
+          as: "types",
+          through: { attributes: [] },
+        },
+      ],
     });
+    if (!project) {
+      throw new Error("Project not found");
+    }
+    return project;
+  }
+
+  async create({
+    name,
+    description,
+    eulogy,
+    causeOfDeath,
+    startDate,
+    endDate,
+    userId,
+    types = [],
+    tombstoneId = null,
+    status = "buried",
+  }) {
+    let transaction;
+    try {
+      transaction = await this.client.transaction();
+      const project = await this.Project.create(
+        {
+          name,
+          description,
+          eulogy,
+          causeOfDeath,
+          startDate,
+          endDate,
+          userId,
+          tombstoneId,
+          status,
+        },
+        { transaction }
+      );
+      await project.setTypes(types || [], { transaction });
+      await transaction.commit();
+
+      return await this.Project.findByPk(project.id, {
+        include: [
+          {
+            model: this.Type,
+            as: "types",
+            through: { attributes: [] },
+          },
+        ],
+      });
+    } catch (error) {
+      if (transaction) await transaction.rollback();
+      console.error("Error starting transaction:", error);
+      throw error;
+    }
   }
 
   async update(id, args) {
@@ -25,6 +90,24 @@ class ProjectService {
       }
     );
     return updated[0] === 1 ? this.getOneId(id) : null;
+  }
+
+  async removeType(id, typeId) {
+    const project = await this.Project.findByPk(id);
+    if (!project) {
+      throw new Error("Project not found");
+    }
+    await project.removeType(typeId);
+    return project;
+  }
+
+  async addType(id, typeId) {
+    const project = await this.Project.findByPk(id);
+    if (!project) {
+      throw new Error("Project not found");
+    }
+    await project.addType(typeId);
+    return project;
   }
 
   async delete(id) {
