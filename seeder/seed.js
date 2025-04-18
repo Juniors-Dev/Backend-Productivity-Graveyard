fd99f611768a6acc6cb185fd66f1803b6ff12726
DavidHansen84 <dracozlair@gmail.com>
2025-04-18 19:06:53 +0200

    Added Achievement to seed

--- seeder/seed.js
+++ seeder/seed.js
@@ -1,28 +1,32 @@
 var { db } = require("../models");
 var userRoles = require("./roles.json");
 var deathTypes = require("./types.json");
+var userAchievements = require("./achievements.json");
 
 async function seed() {
   let transaction;
   try {
     const hasRoles = await db.Role.findAll();
     const hasTypes = await db.Type.findAll();
-    if (hasRoles.length > 0 || hasTypes.length > 0) {
+    const hasAchievements = await db.Achievement.findAll();
+
+    if (hasRoles.length > 0 || hasTypes.length > 0 || hasAchievements.length > 0) {
       console.log("Database already seeded. Skipping seeding process.");
       return;
     }
     transaction = await db.sequelize.transaction();
 
-    const [roles, types] = await Promise.all([
+    const [roles, types, achievements] = await Promise.all([
       db.Role.bulkCreate(userRoles, { transaction }),
       db.Type.bulkCreate(deathTypes, { transaction }),
+      db.Achievement.bulkCreate(userAchievements, { transaction }),
     ]);
 
-    if (!roles || !types) {
-      throw new Error("Failed to seed roles or types.");
+    if (!roles || !types || !achievements) {
+      throw new Error("Failed to seed roles, types or achevements.");
     }
 
-    console.log(roles, types);
+    console.log(roles, types, achievements);
     await transaction.commit();
     console.log("Roles seeded successfully.");
   } catch (error) {
