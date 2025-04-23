62e5c8453d2bffddfc4393fa9b02e4da8ed1f854
Anclagen <91462616+Anclagen@users.noreply.github.com>
2025-04-23 22:41:36 +0200

    validation for dates

--- schema/projectSchema.js
+++ schema/projectSchema.js
@@ -1,12 +1,17 @@
-var { object, string, array, number } = require("yup");
+var { object, string, array, number, date } = require("yup");
 
 const projectSchema = object({
   name: string().required("Name is required"),
   description: string().required("Description is required"),
   eulogy: string().required("Eulogy is required"),
   causeOfDeath: string().required("Cause of death is required"),
-  startDate: string().required("Start date is required"),
-  endDate: string().required("End date is required"),
+  startDate: date().required("Start date is required").typeError("Start date must be a valid date"),
+  endDate: date()
+    .required("End date is required")
+    .typeError("End date must be a valid date")
+    .when("startDate", (startDate, schema) =>
+      startDate ? schema.min(startDate, "End date must be after start date") : schema
+    ),
   types: array().of(number()).required("Types are required"),
   tombstoneId: string().nullable(),
 });
@@ -16,8 +21,12 @@ const projectUpdateSchema = object({
   description: string(),
   eulogy: string(),
   causeOfDeath: string(),
-  startDate: string(),
-  endDate: string(),
+  startDate: date().typeError("Start date must be a valid date"),
+  endDate: date()
+    .typeError("End date must be a valid date")
+    .when("startDate", (startDate, schema) =>
+      startDate ? schema.min(startDate, "End date must be after start date") : schema
+    ),
   tombstoneId: string().nullable(),
   status: string().oneOf(["inactive", "active", "buried", "resurrected", "completed", "archived"]),
 });
