646d3184b69c3b1a281d1bb59c0b0439cd9410d3
Anclagen <91462616+Anclagen@users.noreply.github.com>
2025-04-25 23:51:47 +0200

    filtering implemented in the output

--- services/ProjectService.js
+++ services/ProjectService.js
@@ -8,40 +8,28 @@ class ProjectService {
 
   async getAll(limit = 100, offset = 0, options = {}) {
     const { userId = null, currentUserId = null, status, orderBy, order, types = [] } = options;
-    // return this.Project.findAndCountAll({
-    //   where: {
-    //     ...(orderBy === "createdAt" ? { createdAt: order } : {}),
-    //     ...(userId ? { userId } : {}),
-    //     ...(status ? { status } : {}),
-    //     ...(types.length ? { "$types.id$": types } : {}),
-    //   },
-    //   include: [
-    //     {
-    //       model: this.Type,
-    //       as: "types",
-    //       through: { attributes: [id] },
-    //       //required: !!types.length,
-    //       // ...(types.length ? { where: { id: types } } : {}),
-    //     },
-    //     {
-    //       model: this.User,
-    //       attributes: ["id", "username", "avatarUrl"],
-    //     },
-    //   ],
-    //   limit,
-    //   offset,
-    //   distinct: true,
-    // });
+    const conditions = [];
+
+    if (userId) conditions.push(`p."userId" = :userId`);
+    if (status) conditions.push(`p."status" = :status`);
+    if (types.length) conditions.push(`t."id" IN (:...types)`);
+
+    const whereClause = conditions.length ? `WHERE ${conditions.join(" AND ")}` : "";
+
     const projects = await this.client.query(
       `
       SELECT 
         p."id",
         p."name",
         p."description",
-        p."createdAt",
-        p."updatedAt",
+        p."eulogy",
+        p."causeOfDeath",
+        p."tombstoneId",
+        p."startDate",
+        p."endDate",
+        p."status",
+        ARRAY_AGG(DISTINCT t."name") AS "types",
         COUNT(DISTINCT uv."id") AS "upvoteCount",
-        COUNT(uv."userId") FILTER (WHERE uv."userId" = :currentUserId) AS "userHasVoted",
         MAX(CASE WHEN uv."userId" = :currentUserId THEN 1 ELSE 0 END) > 0 AS "userHasVoted",
         u."username",
         u."avatarUrl",
@@ -51,8 +39,23 @@ class ProjectService {
       LEFT JOIN "ProjectTags" pt ON p."id" = pt."projectId"
       LEFT JOIN "Types" t ON pt."typeId" = t."id"
       LEFT JOIN "Users" u ON p."userId" = u."id"
-      GROUP BY p."id", p."name", p."description", p."createdAt", p."updatedAt", u."username", u."avatarUrl", u."id"
-      ORDER BY p."createdAt" DESC
+      ${whereClause}
+      GROUP BY 
+        p."id",
+        p."name",
+        p."description",
+        p."createdAt",
+        p."updatedAt",
+        u."username",
+        u."avatarUrl",
+        u."id",
+        p."eulogy",
+        p."causeOfDeath",
+        p."tombstoneId",
+        p."startDate",
+        p."endDate",
+        p."status"
+      ${orderBy ? `ORDER BY p."${orderBy}" ${order}` : ""}
       LIMIT :limit OFFSET :offset
       `,
       {
@@ -60,6 +63,11 @@ class ProjectService {
           currentUserId,
           limit,
           offset,
+          userId,
+          status,
+          types,
+          orderBy,
+          order,
         },
         type: this.client.QueryTypes.SELECT,
       }
@@ -67,17 +75,25 @@ class ProjectService {
 
     const count = await this.client.query(
       `
-      SELECT COUNT(*) AS count FROM "Projects" p
+      SELECT COUNT(DISTINCT p.id) AS count FROM "Projects" p
       LEFT JOIN "Upvotes" uv ON p."id" = uv."projectId"
       LEFT JOIN "ProjectTags" pt ON p."id" = pt."projectId"
       LEFT JOIN "Types" t ON pt."typeId" = t."id"
       LEFT JOIN "Users" u ON p."userId" = u."id"
+      ${whereClause}
       `,
       {
+        replacements: {
+          currentUserId,
+          userId,
+          status,
+          types,
+        },
         type: this.client.QueryTypes.SELECT,
       }
     );
 
+    console.log(userId, status, orderBy, order, types);
     return {
       count: parseInt(count[0].count),
       rows: projects,
