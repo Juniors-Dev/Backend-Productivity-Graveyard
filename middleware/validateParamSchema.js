5a964d25c321af8f219622f4a1139a0169c1ae54
Anclagen <91462616+Anclagen@users.noreply.github.com>
2025-05-18 23:36:53 +0200

    updated middleware to new error handler

--- middleware/validateParamSchema.js
+++ middleware/validateParamSchema.js
@@ -1,3 +1,5 @@
+var { createError } = require("../utilities");
+
 const validateParamSchema = (schema) => async (req, res, next) => {
   try {
     await schema.validate(req.params, { abortEarly: false });
@@ -5,17 +7,18 @@ const validateParamSchema = (schema) => async (req, res, next) => {
   } catch (error) {
     console.log(error);
     if (error.name === "ValidationError") {
-      return res.status(400).json({
-        status: "bad request",
+      throw createError({
         statusCode: 400,
-        data: { result: `Validation Error: ${error.message}`, errors: error.errors },
+        status: "bad request",
+        message: `Validation Error: ${error.message}`,
+        errors: error.errors,
       });
     } else {
       // probably going to happen when passing invalid schema
-      return res.status(500).json({
-        status: "internal server error",
+      throw createError({
         statusCode: 500,
-        data: { result: `Internal Server Error: ${error.message}` },
+        status: "internal server error",
+        message: `Internal Server Error: ${error.message}`,
       });
     }
   }
--- middleware/validateSchema.js
+++ middleware/validateSchema.js
@@ -1,20 +1,24 @@
+var createError = require("../utilities/createError");
+var { errorResponse } = require("../utilities/response.js");
+
 const validateSchema = (schema) => async (req, res, next) => {
   try {
     await schema.validate(req.body, { abortEarly: false });
     next();
   } catch (error) {
     if (error.name === "ValidationError") {
-      return res.status(400).json({
-        status: "bad request",
+      throw createError({
         statusCode: 400,
-        data: { result: `Validation Error: ${error.message}`, errors: error.errors },
+        status: "bad request",
+        message: `Validation Error: ${error.message}`,
+        errors: error.errors,
       });
     } else {
       // probably going to happen when passing invalid schema
-      return res.status(500).json({
-        status: "internal server error",
+      throw createError({
         statusCode: 500,
-        data: { result: `Internal Server Error: ${error.message}` },
+        status: "internal server error",
+        message: `Internal Server Error: ${error.message}`,
       });
     }
   }
