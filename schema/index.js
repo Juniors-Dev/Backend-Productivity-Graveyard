1e1671689386201bf0e2c8f3d8eae070fcc22c01
Alexander Barrett <91462616+Anclagen@users.noreply.github.com>
2025-06-02 23:29:03 +0200

    Update schema/index.js
    Co-authored-by: Frida <163577510+fmSand@users.noreply.github.com>

--- schema/index.js
+++ schema/index.js
@@ -3,7 +3,7 @@ const { registerSchema } = require("./registerSchema");
 const { updateUserSchema } = require("./updateUserSchema");
 const { projectIdSchema, commentIdSchema, uuidSchema } = require("./params");
 const { projectSchema, projectUpdateSchema, typeIdSchema } = require("./projectSchema");
-
+const { createCommentSchema, updateCommentSchema } = require("./commentSchema");
 module.exports = {
   loginSchema,
   registerSchema,
