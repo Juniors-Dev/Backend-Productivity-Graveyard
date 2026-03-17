e18bf63f41b2b30d973d18e4e2537b9d15fa9eb7
fmSand <fridamsand@gmail.com>
2026-03-17 21:36:43 +0100

    fix: Inconsistencies between schemas and update barrel file

--- schema/index.js
+++ schema/index.js
@@ -4,6 +4,8 @@ const { updateUserSchema } = require("./updateUserSchema");
 const { projectIdSchema, commentIdSchema, uuidSchema } = require("./params");
 const { projectSchema, projectUpdateSchema, typeIdSchema } = require("./projectSchema");
 const { createCommentSchema, updateCommentSchema } = require("./commentSchema");
+const { updatePasswordSchema, resetPasswordSchema, forgotPasswordSchema } = require("./passwordSchema");
+const { verifyEmailSchema, resetEmailSchema } = require("./emailSchema");
 module.exports = {
   loginSchema,
   registerSchema,
@@ -16,4 +18,9 @@ module.exports = {
   projectSchema,
   projectUpdateSchema,
   typeIdSchema,
+  updatePasswordSchema,
+  resetPasswordSchema,
+  forgotPasswordSchema,
+  verifyEmailSchema,
+  resetEmailSchema,
 };
