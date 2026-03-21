49a72fb47bbf1207944cf2eb936f54bc102e1b63
Frida <163577510+fmSand@users.noreply.github.com>
2026-03-21 11:11:26 +0100

    Add options parameter to getOneId and update methods

--- services/UserService.js
+++ services/UserService.js
@@ -37,7 +37,7 @@ class UserService {
     });
   }
 
-  async getOneId(userId) {
+  async getOneId(userId, options = {}) {
     const user = await this.User.findOne({
       where: { id: userId },
       include: [{ model: this.Role }],
@@ -45,10 +45,12 @@ class UserService {
     });
 
     if (!user) return null;
-    return sanitizeUser(user);
+    return sanitizeUser(user, options);
   }
 
   async getProfile(userId, options = {}) {
+    const { currentUserId = null, ...sanitizeOptions } = options;
+
     const user = await this.User.findOne({
       where: { id: userId },
       include: [{ model: this.Role }],
@@ -57,11 +59,12 @@ class UserService {
 
     if (!user) return null;
 
-    const sanitizedUser = sanitizeUser(user, options);
+    const sanitizedUser = sanitizeUser(user, sanitizeOptions);
     const { count, rows } = await this.projectService.getAll(10, 0, {
       userId,
-      currentUserId: null,
+      currentUserId,
     });
+
     const stats = await this.statsService.getUserStats(userId);
 
     sanitizedUser.projects = {
@@ -90,7 +93,7 @@ class UserService {
     });
   }
 
-  async update(id, args) {
+  async update(id, args, options = {}) {
     const updated = await this.User.update(
       { ...args },
       {
@@ -98,7 +101,7 @@ class UserService {
       }
     );
 
-    const updatedUser = await this.getOneId(id);
+    const updatedUser = await this.getOneId(id, options);
     return updatedUser;
   }
 
