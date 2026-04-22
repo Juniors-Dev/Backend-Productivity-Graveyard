235ab6a37e5c6154d66f1b57072cdf200c0ac3e3
fmSand <fridamsand@gmail.com>
2026-04-22 15:53:20 +0200

    feat: add noUnknown validation to all schema files

--- schema/loginSchema.js
+++ schema/loginSchema.js
@@ -1,8 +1,10 @@
 const yup = require("yup");
 
-const loginSchema = yup.object({
-  email: yup.string().email("Please provide a valid email").required("Email is required"),
-  password: yup.string().required("Please provide a valid password"),
-});
+const loginSchema = yup
+  .object({
+    email: yup.string().email("Please provide a valid email").required("Email is required"),
+    password: yup.string().required("Please provide a valid password"),
+  })
+  .noUnknown(true, "Unknown field in request body");
 
 module.exports = { loginSchema };
